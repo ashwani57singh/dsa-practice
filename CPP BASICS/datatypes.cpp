30be3e3c744700f9b991a8076768ca243b070a89
Ashwani Singh <ashwani57singh@gmail.com>
2026-04-02 00:44:22 +0530

    added datatype file

---
+++ CPP BASICS/datatypes.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // int ,long , long long , float, double
+    // int x;
+    // long x;
+    // long long x;
+    // float x;
+    // double x;
+
+
+    // string and getline
+    //char
+    string str;
+    getline(cin,str);
+    cout<<str;
+    return 0;
+}
+
+// TO SELECT COMPLETE LINE AS INPUT WITHOUT DECLARING MULTIPLE STRINGS
+// string str;
+//     getline(cin,str);
+//     cout<<str;
